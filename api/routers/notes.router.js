6b8b811136e2a87a3fbbac19ebfbde4e837adce7
Vladyslav Kaminskyi <vdkaminskyi@gmail.com>
2019-11-21 21:14:17 +0200

    add middleware for check if parent folder exists in create's router

--- api/routers/notes.router.js
+++ api/routers/notes.router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const controllers = require('../controllers/notes.controller');
+const middleware = require('../middleware/errorHandlers');
 
 const router = Router();
 
@@ -7,7 +8,9 @@ router.get('/all', controllers.getAllNotes);
 
 router
   .route('/:id?')
-  .post(controllers.createNote)
+  .post(
+    middleware.checkIfParentCategoryExists,
+    controllers.createNote)
   .get(controllers.getNoteById)
   .put(controllers.editNote)
   .delete(controllers.deleteNoteById);
