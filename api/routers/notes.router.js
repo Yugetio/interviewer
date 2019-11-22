02497d1a517b83b71183c05e050fe099edf3d41c
Vladyslav Kaminskyi <vdkaminskyi@gmail.com>
2019-11-22 12:10:15 +0200

    fix note's router

--- api/routers/notes.router.js
+++ api/routers/notes.router.js
@@ -9,8 +9,8 @@ router.get('/all', controllers.getAllNotes);
 router
   .route('/:id?')
   .post(middleware.checkIfParentCategoryExists, controllers.createNote)
-  .get(isValidIdFromParams, controllers.getNoteById)
-  .put(isValidIdFromParams, controllers.editNote)
-  .delete(isValidIdFromParams, controllers.deleteNoteById);
+  .get(middleware.isValidIdFromParams, controllers.getNoteById)
+  .put(middleware.isValidIdFromParams, controllers.editNote)
+  .delete(middleware.isValidIdFromParams, controllers.deleteNoteById);
 
 module.exports = router;
