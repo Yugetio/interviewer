06f9e4386912731f144d29f443e27635804f5a70
Vladyslav Kaminskyi <vdkaminskyi@gmail.com>
2019-11-27 13:35:21 +0200

    add login route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,15 +1,10 @@
-const HttpError = require('http-errors');
 const bcrypt = require('bcryptjs');
+const HttpError = require('http-errors');
 const User = require('../models/user.db');
 
 const register = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-  const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) {
-    throw new HttpError[400]('Email already exists');
-  }
 
   const user = await new User({
     email: req.body.email,
@@ -18,11 +13,17 @@ const register = async (req, res) => {
   });
 
   await user.save();
-  res.status(201).json({message: 'user was created'})
+  res.status(201).json({ message: 'user was created' });
 };
 
-const login = (req, res) => {
-  res.json({ msg: 'login' });
+const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new HttpError[400]('Email is not found'); //('Email or password is wrong')
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) throw new HttpError[400]('Invalid password');
+
+  res.send('logged in');
 };
 
 module.exports = {
